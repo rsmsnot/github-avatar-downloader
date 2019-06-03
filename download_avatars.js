bc5e25f70942ec8f0473d63b3e6875fec5a817a2
rsmsnot <rsmsnot@outlook.com>
2019-06-03 23:44:25 +0000

    added authorization

--- download_avatars.js
+++ download_avatars.js
@@ -1,11 +1,19 @@
 var request = require('request');
+var secrets = require('./secrets.js')
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
-    var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
-    request(url, function(err, res, body) {
+    var options = {
+        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+        headers: {
+            'User-Agent': 'request',
+            'Authorization': secrets.GITHUB_TOKEN
+        }
+    };
+
+    request(options, function(err, res, body) {
         cb(err, body);
     });
 }
