9841e856970549d8e340ddbcc026e27c5658ed0e
rsmsnot <rsmsnot@outlook.com>
2019-06-04 00:18:40 +0000

    step 7 - modified callback to return avatar url

--- download_avatars.js
+++ download_avatars.js
@@ -5,20 +5,33 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
+    // var object = JSON.parse();
+    // console.log(object);
+
+
     var options = {
         url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
         headers: {
             'User-Agent': 'request',
             'Authorization': secrets.GITHUB_TOKEN
         }
+
     };
 
     request(options, function(err, res, body) {
-        cb(err, body);
+        cb(err, body); //callback is technically getContributors
     });
+
+
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
     console.log("Errors:", err);
-    console.log("Result:", result);
+    // console.log("Result:", result);
+    var object = JSON.parse(result);
+
+    for (let person of object) {
+        console.log(person.avatar_url);
+
+    }
 });
