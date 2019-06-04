772a14c279abe18531f15fabc1608adebbbc0c70
rsmsnot <rsmsnot@outlook.com>
2019-06-04 01:17:39 +0000

    step 9 & 10 added command line arguments and made them required

--- download_avatars.js
+++ download_avatars.js
@@ -1,13 +1,12 @@
 var request = require('request');
 var secrets = require('./secrets.js')
+var fs = require('fs');
+var args = process.argv.slice(2)
+console.log(args);
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-
-    // var object = JSON.parse();
-    // console.log(object);
-
 
     var options = {
         url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
@@ -15,23 +14,31 @@ function getRepoContributors(repoOwner, repoName, cb) {
             'User-Agent': 'request',
             'Authorization': secrets.GITHUB_TOKEN
         }
-
     };
 
     request(options, function(err, res, body) {
         cb(err, body); //callback is technically getContributors
     });
+}
+if (args[0] && args[1]) {
+    getRepoContributors("jquery", "jquery", function(err, result) {
+        console.log("Errors:", err);
+        // console.log("Result:", result);
+        var object = JSON.parse(result);
 
-
+        for (let person of object) {
+            downloadImageByURL(person.avatar_url, "./avatars/" + person.login + ".jpg")
+            console.log("Downloading... " + person.login);
+        }
+    });
+} else {
+    console.log("No arguments supplied")
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-    console.log("Errors:", err);
-    // console.log("Result:", result);
-    var object = JSON.parse(result);
-
-    for (let person of object) {
-        console.log(person.avatar_url);
-
-    }
-});
+function downloadImageByURL(url, filePath) {
+    request.get(url)
+        .on('error', function(err) {
+            throw err;
+        })
+        .pipe(fs.createWriteStream(filePath))
+}
